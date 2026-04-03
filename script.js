7d98a59fe8518fac5b6b1811a89f9b6165e94ca2
kanhujr <jrkanhu8@gmail.com>
2026-04-03 13:35:29 +0530

    update

--- script.js
+++ script.js
@@ -7,5 +7,14 @@ const btnOpenModal = document.querySelectorAll('.show-modal');
 // console.log(btnOpenModal);
 
 for (let i = 0; i < btnOpenModal.length; i++) {
-  console.log(btnOpenModal[i].textContent);
+  btnOpenModal[i].addEventListener('click', function () {
+    console.log('Button Clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
 }
+
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});

53c01aa605dd065dc72f71d4c4423b91eee2f6a6
kanhujr <jrkanhu8@gmail.com>
2026-04-03 13:40:14 +0530

    update

--- script.js
+++ script.js
@@ -6,15 +6,19 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 // console.log(btnOpenModal);
 
-for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener('click', function () {
-    console.log('Button Clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-}
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-btnCloseModal.addEventListener('click', function () {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
